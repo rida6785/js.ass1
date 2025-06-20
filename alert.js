e216f4fcc018404bff3c41226941934ae3c728b7
rida6785 <rida.abdulraheem309@gmail.com>
2025-06-20 23:58:27 +0500

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,34 @@
+// CHAPTER#01
+// ALERT
+
+
+// QUESTION.1
+alert("welcome to your website");
+
+
+
+// QUESTION.2
+alert("Error! please enter a vaild passwaord.");
+
+
+
+// QUESTION.3
+alert("Welcome to JS land...\n HAPPY CODIND!.");
+
+
+
+// QUESTION.4
+alert("Welcome to JS land....");
+alert("HAPPY CODING!..\n pervent this page from creating aqddtions dailogs.");
+
+
+
+// QUESTION.5
+var message="Hello...I run as JS through my web browser console";
+console.log(message);
+
+
+
+// QUESTION.6/7
+// paractice placement of script element in following sections of your project in exercise 6:
+// the answer question no 7 is a. 
---
+++ variablesforstring.js
@@ -0,0 +1,50 @@
+// CHAPTER#2
+// VARIABLES FOR STRING 
+
+// QUESTION.1
+var username;
+
+
+// QUESTION.2
+var fullname="Rida Abdul Raheem";
+console.log(fullname);
+
+
+// QUESTION.3
+var message="Hello world";
+alert("message");
+
+
+// QUESTION.4
+var studentname="Rida";
+alert("studentname");
+let studentage="20";
+alert ("studentage");
+let studentgender="female";
+alert("studentgender");
+let studenteduation="inter";
+alert("studenteduation");
+
+
+// QUESTION.5
+var pizza="plzzz\nplzz\nplz\npl\np";
+alert(pizza);
+
+
+// QUESTION.6   
+var Email="My email address =ridaabdulraheem.309@email.com";
+alert(Email);
+
+
+// QUESTION.7
+var book="I am trying to learn from the book A smarter way to learn javaScript.";
+alert(book);
+
+
+// QUESTION.8
+document.written("YAH! i can write html content through javascript");
+
+
+// QUESTION.9
+var design=("----------");
+alert(design);
